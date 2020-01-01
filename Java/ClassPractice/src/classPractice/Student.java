7114d778a5d734b67b0c519d7d90ffb6f8476a2f
changgeon <rhckdrjs@gmail.com>
2020-01-01 14:23:01 +0900

    class practice
    class practice

---
+++ Java/ClassPractice/src/classPractice/Student.java
@@ -0,0 +1,13 @@
+package classPractice;
+
+public class Student {
+	
+	public int studentID;
+	public String studentName;
+	public String address;
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address);
+	}
+	
+}
---
+++ Java/ClassPractice/src/classPractice/StudentTest.java
@@ -0,0 +1,15 @@
+package classPractice;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+
+		Student Lee = new Student();
+		Lee.studentName = "Lee";
+		Lee.address = "Daejeon";
+		
+		Lee.showStudentInfo();
+		
+	}
+
+}

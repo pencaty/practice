0ab7296c6d399593a72eb0c11346e02cc8b67e3e
changgeon <rhckdrjs@gmail.com>
2020-01-01 15:34:38 +0900

    class and function practice
    class and function practice

--- Java/ClassPractice/src/classPractice/Student.java
+++ Java/ClassPractice/src/classPractice/Student.java
@@ -5,9 +5,22 @@ public class Student {
 	public int studentID;
 	public String studentName;
 	public String address;
+	public boolean graduated;
 	
-	public void showStudentInfo() {
+	public Student() {}
+	
+	public Student(int id, String name) {
+		studentID = id;
+		studentName = name;
+		address = "No Address";
+	}
+	
+	public void showStudentInfo() { // method
 		System.out.println(studentName + "," + address);
+	}
+	
+	public String getStudentName() {
+		return studentName;
 	}
 	
 }
--- Java/ClassPractice/src/classPractice/StudentTest.java
+++ Java/ClassPractice/src/classPractice/StudentTest.java
@@ -1,6 +1,24 @@
 package classPractice;
 
 public class StudentTest {
+	
+	public static int addNum(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	public static void sayHello(String greeting) {
+		System.out.println(greeting);
+	}
+	
+	public static int calcSum() {
+		int sum = 0, i;
+		
+		for(i=0; i<=100;i++) {
+			sum += i;
+		}
+		
+		return sum;
+	}
 
 	public static void main(String[] args) {
 
@@ -9,6 +27,15 @@ public class StudentTest {
 		Lee.address = "Daejeon";
 		
 		Lee.showStudentInfo();
+		
+		Student Son = new Student(172, "Son");
+		Son.showStudentInfo();
+		
+		System.out.println(addNum(3,5));
+		
+		sayHello("Hello Java");
+		
+		System.out.println(calcSum());
 		
 	}
 

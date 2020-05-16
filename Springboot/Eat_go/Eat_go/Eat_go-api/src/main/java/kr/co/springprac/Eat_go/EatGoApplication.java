24316eed72af03888cd6e78f277285249783c7cc
changgeon <rhckdrjs@gmail.com>
2020-05-17 00:00:13 +0900

    Create Eat_go
    Create Springboot project Eat_go

---
+++ Springboot/Eat_go/Eat_go/Eat_go-api/src/main/java/kr/co/springprac/Eat_go/EatGoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.springprac.Eat_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatGoApplication.class, args);
+	}
+
+}
---
+++ Springboot/Eat_go/Eat_go/Eat_go-api/src/test/java/kr/co/springprac/Eat_go/EatGoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.springprac.Eat_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
